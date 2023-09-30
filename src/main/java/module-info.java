30997aae41d31858f6102d247acda655cf68ebd6
paha_koha <pashagerasimik@mail.ru>
2023-09-30 19:57:23 +0300

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.codex {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.example.codex to javafx.fxml;
+    exports com.example.codex;
+}
