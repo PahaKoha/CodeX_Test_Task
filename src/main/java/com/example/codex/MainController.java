82d9ac7e7bd7acb8e2ecbc586fe0d1cd2daddeee
paha_koha <pashagerasimik@mail.ru>
2023-09-30 20:19:04 +0300

    Added login menu

--- src/main/java/com/example/codex/MainController.java
+++ src/main/java/com/example/codex/MainController.java
@@ -1,14 +1,38 @@
 package com.example.codex;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button logInButton;
+
+    @FXML
+    private TextField logInField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Button singUpButton;
+
+    @FXML
+    void initialize() {
+        assert logInButton != null : "fx:id=\"logInButton\" was not injected: check your FXML file 'MainView.fxml'.";
+        assert logInField != null : "fx:id=\"logInField\" was not injected: check your FXML file 'MainView.fxml'.";
+        assert passwordField != null : "fx:id=\"passwordField\" was not injected: check your FXML file 'MainView.fxml'.";
+        assert singUpButton != null : "fx:id=\"singUpButton\" was not injected: check your FXML file 'MainView.fxml'.";
+
     }
+
 }
